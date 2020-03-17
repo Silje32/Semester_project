50e7ec00a66cf3140d1484362b250eaa74ec9d77
Silje Heldal Monsen <Silje24@yahoo.com>
2020-03-17 21:53:24 +0100

    added form.js to the contact page. Started the validation

---
+++ javascript/form.js
@@ -0,0 +1,59 @@
+//Use RegEx to validate form
+
+//Select the form:
+const form = document.querySelector("contact");
+
+// Submit event
+function validateForm(event) {
+  alert ("The form was submitted");
+}
+
+form.addEventListener("submit", validateForm);
+
+
+
+//Validating that the full name input are not empty.
+function checkFullName(fullname) {
+  var myName = /(\w+)/;
+  if (fullname.match(myName)) {
+     return true;
+  }  else {
+     alert( "Please enter your name and last name here" );
+     return false;
+  }
+}
+
+
+//Validating that the e-mail address is correct.
+function checkEmail(email) {
+  var myEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (email.match(myEmail)) {
+     return true;
+  }  else {
+     alert( "The e-mail number entered is invalid!" );
+     return false;
+  }
+}
+
+
+//Validating that the phone number is correct.
+function checkPhone(phone) {
+  var myPhone =   /^\d{8}$/;
+  if (phone.match(myPhone)) {
+     return true;
+  }  else {
+     alert( "The phone number entered is invalid!" );
+     return false;
+  }
+}
+
+
+
+//Function to check that the submit button is working.
+
+form.addEventListener("submit", validateForm);
+
+function validateForm(event) {
+    event.preventDefault();
+    alert("the form was successfully submitted");
+}
