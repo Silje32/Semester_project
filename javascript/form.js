eeeba56926a1512c2c1b049430fc0bb3ce748662
Silje Heldal Monsen <Silje24@yahoo.com>
2020-03-22 21:55:13 +0100

    added validation to contact page, form.js and css

--- javascript/form.js
+++ javascript/form.js
@@ -1,52 +1,60 @@
 //Use RegEx to validate form
 
-//Select the form:
-const form = document.querySelector("contact");
+//Validating that "full name" input are not empty.
+function validateFullName(name) {
+	 var firstNameError = document.querySelector("#nameError");
 
-// Submit event
-function validateForm(event) {
-  alert ("The form was submitted");
-}
+	 if (name.length === 0) {
+			 firstNameError.style.display = "block";
+	 }
+	 else {
+			 firstNameError.style.display = "none";
+	 }
+ }
 
-form.addEventListener("submit", validateForm);
+//Validating that the e-mail adress is correct.
+function validateEmail(email) {
+  var emailError = document.querySelector("#emailError");
+  var emailPattern = /\S+@\S+.\S+/;
 
-
-
-//Validating that the full name input are not empty.
-function checkFullName(fullname) {
-  var myName = /(\w+)/;
-  if (fullname.match(myName)) {
-     return true;
-  }  else {
-     alert( "Please enter your name and last name here" );
-     return false;
+/* The exclamation mark checks if the pattern doesn't match so if it doesn't
+  match display the error otherwise hide the error */
+  if (!email.match(emailPattern)) {
+    emailError.style.display = "block";
   }
-}
-
-
-//Validating that the e-mail address is correct.
-function checkEmail(email) {
-  var myEmail = /\S+@\S+.\S+/;
-  if (email.match(myEmail)) {
-     return true;
-  }  else {
-     alert( "The e-mail number entered is invalid!" );
-     return false;
+  else {
+    emailError.style.display = "none";
   }
 }
 
 
 //Validating that the phone number is correct.
-function checkPhone(phone) {
-  var myPhone =   /^\d{8}$/;
-  if (phone.match(myPhone)) {
-     return true;
-  }  else {
-     alert( "The phone number entered is invalid!" );
-     return false;
-  }
+ function validatePhone(phone) {
+	var phoneError = document.querySelector("#phoneError");
+	var phonePattern = /^\d{3}\ \d{3}\ \d{4}$/;
+
+/* The exclamation mark checks if the pattern doesn't match so if it doesn't
+   match display the error otherwise hide the error */
+	if (!phone.match(phonePattern)) {
+		phoneError.style.display = "block";
+	}
+	else {
+		phoneError.style.display = "none";
+	}
 }
 
+
+//The function will be called inside the form's eventListener
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+
+  event.preventDefault();
+
+  validateFullName(this.name.value);
+  validatePhone(this.phone.value);
+  validateEmail(this.email.value);
+});
 
 
 //Function to check that the submit button is working.
